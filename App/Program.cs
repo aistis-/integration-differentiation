f053cec1b592f4e0520ba935da28f5084c03153d
Aistis <aistis8624@gmail.com>
2014-05-17 16:31:16 +0300

    Added trapecoidal method

--- App/Program.cs
+++ App/Program.cs
@@ -8,8 +8,21 @@ namespace App
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            double a = 1;
+            double b = 9;
+
+            int N = 10000;
+
+            TrapezoidalRule.calculate(a, b, N);
+
+            Console.ReadKey();
+        }
+
+        public static double calculateWithFormula(double x)
+        {
+            return Math.Sqrt(x) * (1 + Math.Sqrt(x));
         }
     }
 }
---
+++ App/TrapezoidalRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class TrapezoidalRule
+    {
+        public static void calculate(double a, double b, int N)
+        {
+            double result = calculateSum(a, b, N);
+            double resultForMismatch = calculateSum(a, b, 2 * N);
+
+            Console.WriteLine("Calculated with trapezoidal rule: " + result);
+
+            double misMatch = Math.Abs(result - resultForMismatch) / 3;
+
+            Console.WriteLine("Mismatch by Runge's rule: " + misMatch);
+        }
+
+        private static double calculateSum(double a, double b, int N)
+        {
+            double sum = 0;
+            double h = (b - a) / (N - 1); // step size
+
+
+            for (int i = 0; i < N; i++)
+            {
+                sum += h * (Program.calculateWithFormula(a + i * h) + Program.calculateWithFormula(a + (i + 1) * h)) / 2;
+            }
+
+            return sum;
+        }
+    }
+}
