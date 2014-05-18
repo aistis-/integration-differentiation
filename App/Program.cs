4b626de5ed6c12139de8c019edcce39efd7533a3
Aistis <aistis8624@gmail.com>
2014-05-18 21:24:29 +0300

    Added Runge and Kutta method for differentiation

--- App/Program.cs
+++ App/Program.cs
@@ -37,12 +37,34 @@ namespace App
                 Console.WriteLine("N = {0} result: {1} Runge's rule: {2}", N[i], result, misMatch);
             }
 
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Calculations with Runge and Kutta method");
+
+            double[] h = {0.1, 0.05, 0.025, 0.0125};
+            a = 0;
+            b = 1;
+
+            for (int i = 0; i < h.Length; i++)
+            {
+                Console.WriteLine();
+                result = RungeKutta.Calculate(a, b, h[i], true);
+                misMatch = Math.Abs(RungeKutta.Calculate(a, b, h[i] / 2.0, false) - result) / (Math.Pow(2, 2) - 1);
+                Console.WriteLine("h = {0} result: {1} Runge's rule: {2}", h[i], result, misMatch);
+            }
+
             Console.ReadKey();
         }
 
         public static double Function(double x)
         {
             return Math.Sqrt(x) * (1 + Math.Sqrt(x));
+        }
+
+        public static double Function2(double x, double u)
+        {
+            return -u + Math.Sin(x);
+            //return (0.7 - Math.Pow(u, 2)) * Math.Cos(x) + 0.3 * u;
         }
     }
 }
---
+++ App/RungeKutta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Globalization;
+
+namespace App
+{
+    class RungeKutta
+    {
+        public static double Calculate(double a, double b, double h, bool print)
+        {
+            double u = 1;
+            double x = a;
+
+            while (x <= b)
+            {
+                double k1 = Program.Function2(x, u);
+                double k2 = Program.Function2(x + h / 2.0, u + h * k1 / 2.0);
+
+                if (print)
+                {
+                    Console.Write("(" + Math.Round(x, 4).ToString(CultureInfo.CreateSpecificCulture("en-GB"))
+                        + "," + Math.Round(u, 4).ToString(CultureInfo.CreateSpecificCulture("en-GB")) + ")");
+                }
+
+                x += h;
+                u += h * k2;
+            }
+
+            if (print)
+            {
+                Console.WriteLine();
+            }
+            
+            return u;
+        }
+    }
+}
